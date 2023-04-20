d381885bc2be26741e0bc81f298264420d96a812
BronzeOwl4 <yoyomartha92@gmail.com>
2023-04-20 12:47:28 -0700

    Task last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
   * main - Entry point
   *
-  * Return: Always 0 (Success)
+  * Return: Always 1 (Success)
   */
 int main(void)
 {
-	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	for (int i = 0; text[i] != '\0'; i++) {
-		putchar(text[i]);
-	}
-	putchar('\n');
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
