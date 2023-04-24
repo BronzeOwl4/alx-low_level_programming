eca0f61021d05d682751fa44d95239a1a0aefba0
BronzeOwl4 <yoyomartha92@gmail.com>
2023-04-24 06:49:56 -0700

    Task 9

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,11 +10,16 @@ int main(void)
 	
 	for (i = 0; i < 16; i++)
 	{
-		if (i < 10)
-			putchar(i + '0');
-		else
-			putchar(i - 10 + 'a');
+	if (i < 10)
+	{
+		putchar(i + '0');
+	}
+	else
+	{
+		putchar(i - 10 + 'a');
+	}
 	}
 	putchar('\n');
 	return (0);
+
 }
