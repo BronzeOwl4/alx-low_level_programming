063ba889bc3930565f4735967e303c382b376c3e
BronzeOwl4 <yoyomartha92@gmail.com>
2023-04-25 17:56:10 -0700

    Task 14

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success(
+ */
+int main(void)
+{
+	int i = 3;
+	int a = 1, b = 2, c;
+
+	printf("%d, %d", a, b);
+	while (i <= 98)
+	{
+		c = a + b
+			printf(", %d", c);
+		a = b;
+		b = c;
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
