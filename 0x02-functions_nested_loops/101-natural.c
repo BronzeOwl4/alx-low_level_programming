90e85f6cf757aeb0cc2baabf5f1d510c59e9f927
BronzeOwl4 <yoyomartha92@gmail.com>
2023-04-25 17:04:18 -0700

    Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,13 +8,16 @@
 int main(void)
 {
 	int sum = 0;
+	int i = 0;
 
-	for (int i = 0; i < 1024; i++)
+	while (i < 1024)
 	{
 		if (i % 3 == 0 ||  i % 5 == 0)
 		{
 			sum += i;
 		}
+		i++;
 	}
 	printf("%d\n", sum);
+	return (0);
 }
