17c0ea68919ac436c7fd2575c053b48e876802aa
BronzeOwl4 <yoyomartha92@gmail.com>
2023-04-27 18:05:17 -0700

    Task

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,15 +9,12 @@
  */
 void print_line(int n)
 {
-	if (n > 0)
-	{
-		int i = 0;
+	int i = 0;
 
-		while (i < n)
-		{
-			_putchar('_');
-			i++;
-		}
-		_putchar('\n');
+	while (i < n)
+	{
+		_putchar('_');
+		i++;
 	}
+	_putchar('\n');
 }
