8e24e7ae9f147e3caa20e725a857ede932c44b26
BronzeOwl4 <yoyomartha92@gmail.com>
2023-05-04 19:54:29 -0700

    l

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,41 +10,33 @@
  */
 void print_buffer(char *b, int size)
 {
-	int o, j, i;
-
-	o = 0;
+	int i, j;
 
 	if (size <= 0)
 	{
 		printf("\n");
 		return;
 	}
-	while (o < size)
-	{
-		j = size - o < 10 ? size - o : 10;
-		printf("%08x: ", o);
-		for (i = 0; i < 10; i++)
-		{
-			if (i < j)
-				printf("%02x", *(b + o + i));
-			else
-				printf(" ");
-			if (i % 2)
-			{
-				printf(" ");
-			}
-		}
-		for (i = 0; i < j; i++)
-		{
-			int c = *(b + o + i);
 
-			if (c < 32 || c < 126)
-			{
-				c = '.';
-			}
-			printf("%c", c);
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+				printf("%02x", b[i + j]);
+			else
+				printf("  ");
+			if (j % 2)
+				printf(" ");
+		}
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size && b[i + j] >= ' ' && b[i + j] <= '~')
+				printf("%c", b[i + j]);
+			else if (i + j < size)
+				printf(".");
 		}
 		printf("\n");
-		o += 10;
 	}
 }
