824c52fb7546e03cefb7e3579182781e32ccb090
BronzeOwl4 <yoyomartha92@gmail.com>
2023-05-18 13:12:36 -0700

    l

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,33 +11,24 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-	char *ptr;
+	unsigned long mul;
+	int i, j;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	for (ptr = argv[1]; *ptr; ptr++)
+	for (i = 1; i < argc; i++)
 	{
-		if (!isdigit(*ptr))
-		{
-			printf("Error\n");
-			exit(98);
-		}
+		for (j = 0; argv[i][j] != '\0'; j++)
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{
+				printf("Error\n");
+				exit (98);
+			}
 	}
-	for (ptr = argv[2]; *ptr; ptr++)
-	{
-		if (!isdigit(*ptr))
-		{
-			printf("Error\n");
-			exit(98);
-		}
-	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-
-	printf("%d\n", num1 * num2);
+	mul = atol(argv[1]) *atol(argv[2]);
+	printf("%lu\n", mul);
 	return (0);
 }
