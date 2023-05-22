846b9bb19bc12929aa20ff3405b91da2332d7227
BronzeOwl4 <yoyomartha92@gmail.com>
2023-05-22 10:16:10 -0700

    h

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,5 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * new_dog - creates a new dog
@@ -13,19 +11,27 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog = malloc(sizeof(dog_t));
+	dog_t *new_dog;
+	int i, name_len, owner_len;
+
+	new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = strdup(name);
-
+	for (name_len = 0; name[name_len]; name_len++)
+		;
+	new_dog->name = malloc((name_len + 1) * sizeof(char));
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
+	for (i = 0; i <= name_len; i++)
+		new_dog->name[i] = name[i];
 	new_dog->age = age;
-	new_dog->owner = strdup(owner);
+	for (owner_len = 0; owner[owner_len]; owner_len++)
+		;
+	new_dog->owner = malloc((owner_len + 1) * sizeof(char));
 
 	if (new_dog->owner == NULL)
 	{
@@ -33,5 +39,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
+	for (i = 0; i <= owner_len; i++)
+		new_dog->owner[i] = owner[i];
 	return (new_dog);
 }
