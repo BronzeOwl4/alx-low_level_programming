113d7b57900fc22fa8ad5b60432da768aae903be
BronzeOwl4 <yoyomartha92@gmail.com>
2023-06-06 18:40:54 -0700

    l

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,23 +1,50 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* reverse_listint - reverses a linked list.
-* @head: head of a list.
-*
-* Return: a pointer to the first node of the reversed list.
-*/
-listint_t *reverse_listint(listint_t **head)
+ * print_listint_safe - prints a listint_t linked list
+ * @head: pointer to the head of the list
+ *
+ * Return: the number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
 {
-	listint_t *prev = NULL;
-	listint_t *next;
+	size_t count = 0;
+	const listint_t *temp;
+	listp_t *new_node, *hptr = NULL;
 
-	while (*head != NULL)
+	while (head)
 	{
-		next = (*head)->next;
-		(*head)->next = prev;
-		prev = *head;
-		*head = next;
+		new_node = malloc(sizeof(listp_t));
+		if (!new_node)
+			exit(98);
+		new_node->p = (void *)head;
+		new_node->next = hptr;
+		hptr = new_node;
+		temp = head;
+		head = head->next;
+
+		for (new_node = hptr->next; new_node; new_node = new_node->next)
+			if (new_node->p == head)
+			{
+				printf("-> [%p] %d\n", (void *)head, temp->n);
+				while (hptr)
+				{
+					temp = (const listint_t *)hptr->p;
+					free(hptr);
+					hptr = hptr->next;
+				}
+				return (count);
+			}
+		printf("[%p] %d\n", (void *)temp, temp->n);
+		count++;
 	}
-	*head = prev;
-	return (*head);
+	while (hptr)
+	{
+		temp = (const listint_t *)hptr->p;
+		free(hptr);
+		hptr = hptr->next;
+	}
+	return (count);
 }
