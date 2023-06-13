396f7a1b6f1c688feed29e5865c5564a9e48677a
BronzeOwl4 <yoyomartha92@gmail.com>
2023-06-12 19:34:17 -0700

    Done

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <elf.h>
+#include <fcntl.h>
 
 #define ERROR_MSG "Error: Not an ELF file\n"
 
@@ -20,12 +21,14 @@ void print_header_info(Elf64_Ehdr header);
  */
 int main(int argc, char *argv[])
 {
+	int fd;
+	Elf64_Ehdr header;
+
 	if (argc != 2)
 	{
 		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
 		return (98);
 	}
-	int fd;
 
 	fd = open(argv[1], O_RDONLY);
 
@@ -34,7 +37,6 @@ int main(int argc, char *argv[])
 		perror("Error");
 		return (98);
 	}
-	Elf64_Ehdr header;
 
 	if (read(fd, &header, sizeof(header)) != sizeof(header))
 	{
@@ -61,24 +63,20 @@ int main(int argc, char *argv[])
  */
 void print_header_info(Elf64_Ehdr header)
 {
+	int i;
+
 	printf("Magic:   ");
-	for (int i = 0; i < EI_NIDENT; i++)
+	for (i = 0; i < EI_NIDENT; i++)
 	{
 		printf("%02x ", header.e_ident[i]);
 	}
 	printf("\nClass:  %s\n",
 			header.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
 	printf("Data:   %s\n",
-			header.e_ident[EI_DATA]
-			== ELFDATA2LSB ? "2's complement, little endian" :
-			header.e_ident[EI_DATA]
-			== ELFDATA2MSB ? "2's complement, big endian" :
+			header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" :
+			header.e_ident[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" :
 			"Invalid data encoding");
 	printf("Version:  %d\n", header.e_ident[EI_VERSION]);
 	printf("OS/ABI:  %d\n", header.e_ident[EI_OSABI]);
 	printf("ABI Version:  %d\n", header.e_ident[EI_ABIVERSION]);
-	printf("Type:  %d\n", header.e_type);
-	printf("Entry point address:  %lx\n", header.e_entry);
-	printf("Format:  %d\n", header.e_ident[EI_CLASS]
-			== ELFCLASS64 ? ELF64_FORMAT : ELF32_FORMAT);
 }
